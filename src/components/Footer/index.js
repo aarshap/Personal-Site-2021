2827943ec9ffc569d82fe9c82ff94360b55bb84e
Aaron Shapiro <aaron@aaron.mn>
2021-12-21 19:44:45 -0500

    Removed looking for work copy, LinkedIn link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ export default () => {
                     <h3>
                         <em>Thanks for visiting.</em>
                     </h3>
-                    <h3>I'm looking for my next full-time role. If you'd like to work together, say hi.</h3>
+                    <h3>I'm not currently looking for freelance or full-time work, but feel free to say hi.</h3>
                     <h3>
                         <a className="inverted" href="mailto:aaron@aaron.mn">aaron@aaron.mn</a>
                     </h3>
@@ -24,7 +24,6 @@ export default () => {
                         <Styled.Links>
                             <a className="inverted" href="https://twitter.com/aaronshapiro">Twitter</a>
                             <a className="inverted" href="https://twitch.tv/okayaaron">Twitch</a>
-                            <a className="inverted" href="https://www.linkedin.com/in/aaron-shapiro-976922109/">LinkedIn</a>
                             <a className="inverted" href="https://github.com/aarshap/">GitHub</a>
                         </Styled.Links>
                     </Styled.SecondaryInner>
