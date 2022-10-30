2ff8f0daa9471c163d82b52da2fa32c209b81c21
Aaron Shapiro <aaron@aaron.mn>
2022-10-30 18:24:29 -0400

    Revert "Rebuilding.."
    This reverts commit f84e9bc021b0bac4c6de563ee23b1928663cf96f.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,30 @@
 import React from "react"
+import WritingList from "../components/WritingList"
+import Head from "../components/Head"
+import Hero from "../components/Hero"
+import Layout from "../components/Layout"
+import PhotographyList from "../components/PhotographyList"
+import WorkList from "../components/WorkList"
 
 const Index = () => {
     return (
-        <>
-            Something new soon.
-        </>
+        <Layout>
+            <Head title="Home" />
+            <section>
+                <Hero />
+            </section>
+            <div className="container">
+                <section>
+                    <WorkList />
+                </section>
+                <section>
+                    <WritingList />
+                </section>
+                <section>
+                    <PhotographyList />
+                </section>
+            </div>
+        </Layout>
     )
 }
 
